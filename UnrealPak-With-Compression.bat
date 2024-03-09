44aea39459bbbf582e135079ebcf7e1fd32b13c5
btwOreO <153916501+btwOreO@users.noreply.github.com>
2024-03-09 21:49:04 +0530

    Add files via upload

---
+++ UnrealPak-With-Compression.bat
@@ -0,0 +1,10 @@
+@if "%~1"=="" goto skip
+
+@setlocal enableextensions
+@pushd %~dp0
+@echo "%~1\*.*" "..\..\..\*.*" >filelist.txt
+.\UnrealPak.exe "%~1.pak" -create=filelist.txt -compress
+@popd
+@pause
+
+:skip
